78da63529f0cddcdabf35b9d0862a4fb9bf814c1
Joaquin Lago <joaquinlago853@gmail.com>
2025-06-30 20:46:16 -0300

    Se agrega operacion get para todos los mensajes

---
+++ main_api.py
@@ -0,0 +1,11 @@
+from typing import Optional
+from pydantic import BaseModel, EmailStr
+
+class Persona(BaseModel):
+    id: Optional[int] = None
+    user: str
+    mensaje: textodelmensaje(texto)str
+
+    
+    
+    
