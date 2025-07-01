42a45683c47701b8df75253023b622f3c7e64044
Joaquin Lago <joaquinlago853@gmail.com>
2025-06-30 21:08:07 -0300

    Se agrega operacion get por id usando modelo persona

--- main_api.py
+++ main_api.py
@@ -1,11 +1,24 @@
 from typing import Optional
 from pydantic import BaseModel, EmailStr
 
+app=FastAPI()
+
 class Persona(BaseModel):
     id: Optional[int] = None
     user: str
-    mensaje: textodelmensaje(texto)str
+    mensaje: str
 
-    
-    
-    
+mensajes_db= []
+
+@app.get("/mensajes")    
+def get_mensajes():
+    return mensajes_db
+
+from fastapi import HTTPException
+
+@app.get("/mensajes/{mensaje_id}")
+def get_mensaje(mensaje_id: int):
+    for mensaje in mensajes_db:
+        if mensaje.id == mensaje_id:
+            return mensaje
+    raise HTTPException(status_code=404, detail="Mensaje no encontrado")
