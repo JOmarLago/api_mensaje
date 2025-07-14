543fd85a602298c612f67524e2b87a1aedcc01d2
Joaquin Lago <joaquinlago853@gmail.com>
2025-07-13 21:43:03 -0300

    Agregar operacion PUT para actualizar mensaje

--- main_api.py
+++ main_api.py
@@ -28,3 +28,21 @@ def crear_mensaje(mensaje: Persona):
     mensaje.id = len(mensajes_db) + 1
     mensajes_db.append(mensaje)
     return mensaje
+
+
+@app.put("/mensajes/{mensaje_id}")
+def actualizar_mensaje(mensaje_id: int, mensaje_actualizado: Persona):
+    for i, mensaje in enumerate(mensajes_db):
+        if mensaje.id == mensaje_id:
+            mensaje_actualizado.id = mensaje_id
+            mensajes_db[i] = mensaje_actualizado
+            return mensaje_actualizado
+    raise HTTPException(status_code=404, detail="Mensaje no encontrado")
+
+@app.delete("/mensajes/{mensaje_id}")
+def eliminar_mensaje(mensaje_id: int):
+    for i, mensaje in enumerate(mensajes_db):
+        if mensaje.id == mensaje_id:
+            del mensajes_db[i]
+            return {"mensaje": f"Mensaje con ID {mensaje_id} eliminado"}
+    raise HTTPException(status_code=404, detail="Mensaje no encontrado")
